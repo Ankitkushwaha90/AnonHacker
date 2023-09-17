1cdc2b4edf635c0936f15a73f3a7cdcabe19cf7a
Ankit kushwaha <119472022+Ankitkushwaha90@users.noreply.github.com>
2023-09-18 04:21:05 +0530

    Update Home.js
    yejaofjs

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () =>{
         return(
             <>
-                <h1>Welcome to Home page</h1>
+                <h1>Welcome to Home page I am latest</h1>
             </>
         );
 }
